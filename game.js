2a81a0fa75da22bc80ed58fa6ce2bb0f6e797ade
Lee Inji <90603357+injilee@users.noreply.github.com>
2022-03-02 16:11:59 +0900

    🎉 프로젝트 시작

---
+++ game.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const btn = document.querySelector('.startBtn');
+const stopIcon = document.querySelector('.startBtn i');
+
+startGame(){
+    if(stopIcon.classList ){
+
+    }
+};
+
+btn.addEventListener('toggle', startGame);
