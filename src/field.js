c1d7a622b67cf663bea4abb54ae49a634cf02733
Lee Inji <lij8016@gmail.com>
2023-03-07 23:33:18 +0900

    ♻️ refactoring code

--- src/field.js
+++ src/field.js
@@ -3,85 +3,90 @@
 const ITEM_SIZE = 50;
 
 export const ItemName = Object.freeze({
-    bug : 'bug',
-    carrot : 'carrot',
+   bug: 'bug',
+   carrot: 'carrot',
+});
+
+const field = document.querySelector('.play-station');
+
+export default class Field {
+   constructor(bugCount, carrotCount) {
+      window.addEventListener('resize', () => {
+         this.fieldRect = field.getBoundingClientRect();
+         this.fieldWidth = this.fieldRect.width - ITEM_SIZE;
+         this.fieldHeight = this.fieldRect.height - ITEM_SIZE;
+      });
+      this.bugCount = bugCount;
+      this.carrotCount = carrotCount;
+      this.field = document.querySelector('.play-station');
+      this.fieldRect = field.getBoundingClientRect();
+      // this.onClick = this.onClick.bind(this);
+      // this.field.addEventListener('click', (event) => this.onClick(event));
+      this.field.addEventListener('click', this.onClick);
+      this.fieldWidth = this.fieldRect.width - ITEM_SIZE;
+      this.fieldHeight = this.fieldRect.height - ITEM_SIZE;
+      this.timer = undefined;
+   }
+
+   setClickListener(clickField) {
+      this.clickField = clickField;
+   }
+
+   onClick = event => {
+      // console.log(event.target);
+      this.clickField && this.clickField(event.target);
+   };
+
+   init() {
+      this.field.innerHTML = '';
+      this._addItem('bug__items', this.bugCount, './resource/img/bug.png');
+      this._addItem('carrot__items', this.carrotCount, './resource/img/carrot.png');
+      this.move(this.fieldWidth, this.fieldHeight);
+   }
+
+   _addItem(className, count, imgSrc) {
+      const x1 = 0;
+      const y1 = 0;
+
+      for (let i = 0; i < count; i++) {
+         const items = document.createElement('img');
+         items.setAttribute('class', className);
+         items.setAttribute('src', imgSrc);
+         const x = randomLocation(x1, this.fieldWidth);
+         const y = randomLocation(y1, this.fieldHeight);
+         randomLocation(x, y);
+         items.style.left = `${x}px`;
+         items.style.top = `${y}px`;
+         this.field.append(items);
+      }
+   }
+   move(x, y) {
+      const bugs = document.querySelectorAll('.bug__items');
+      this.timer = setInterval(() => {
+         bugs.forEach(bug => {
+            const x1 = randomLocation(-50, 50);
+            const y1 = randomLocation(-50, 50);
+
+            let bugX = parseFloat(bug.style.left);
+            let bugY = parseFloat(bug.style.top);
+            bugX += x1;
+            bugY += y1;
+
+            if (bugX > 0 && bugX < x) {
+               bug.style.left = `${bugX}px`;
+            }
+            if (bugY > 0 && bugY < y) {
+               bug.style.top = `${bugY}px`;
+            }
+            bug.style.transition = 'all ease 1000ms';
+         });
+      }, 100);
+   }
+   moveStop() {
+      clearInterval(this.timer);
+   }
 }
-);
 
-export default class Field{
-    constructor(bugCount, carrotCount) {
-        this.bugCount = bugCount;
-        this.carrotCount = carrotCount;
-        this.field = document.querySelector('.play-station');
-        this.fieldRect = this.field.getBoundingClientRect();
-        // this.onClick = this.onClick.bind(this);
-        // this.field.addEventListener('click', (event) => this.onClick(event));
-        this.field.addEventListener('click', this.onClick);
-
-        this.timer = undefined;
-        this.fieldWidth = this.fieldRect.width - ITEM_SIZE;
-        this.fieldHeight = this.fieldRect.height - ITEM_SIZE;
-    }
-
-    setClickListener(clickField){
-        this.clickField = clickField;
-    }
-
-    onClick = event =>{
-        // console.log(event.target);
-        this.clickField && this.clickField(event.target);
-    }
-
-    init(){
-        this.field.innerHTML = '';
-        this._addItem('bug__items', this.bugCount, './resource/img/bug.png');
-        this._addItem('carrot__items', this.carrotCount, './resource/img/carrot.png');
-        this.move(this.fieldWidth, this.fieldHeight);
-    }
-
-    _addItem(className, count, imgSrc){
-        const x1 = 0;
-        const y1 = 0;
-    
-        for(let i = 0;i < count; i++){
-            const items = document.createElement('img');
-            items.setAttribute('class', className);
-            items.setAttribute('src', imgSrc);
-            const x = randomLocation(x1, this.fieldWidth);
-            const y = randomLocation(y1, this.fieldHeight);
-            randomLocation(x, y);
-            items.style.left = `${x}px`;
-            items.style.top = `${y}px`;
-            this.field.append(items);
-        }
-    }
-    move(x, y){
-        const bugs = document.querySelectorAll('.bug__items');
-        this.timer = setInterval(() => {
-            bugs.forEach((bug) => {
-                const x1 = randomLocation(-50, 50);
-                const y1 = randomLocation(-50, 50);
-
-                let bugX = parseFloat(bug.style.left);
-                let bugY = parseFloat(bug.style.top);
-                bugX += x1;
-                bugY += y1;
-
-                if(bugX > 0 && bugX < x){
-                    bug.style.left = `${bugX}px`;
-                }
-                if(bugY > 0 && bugY < y){
-                    bug.style.top = `${bugY}px`;
-                }
-                bug.style.transition = 'all ease 1000ms';
-            })
-        }, 100);
-    }
-    moveStop(){
-        clearInterval(this.timer);
-    }
-}
-
-function randomLocation(x, y){
-    return Math.random() * (y - x) + x;
+function randomLocation(x, y) {
+   return Math.random() * (y - x) + x;
 }
--- src/game.js
+++ src/game.js
@@ -6,155 +6,147 @@ import * as sound from './sound.js';
 
 // Object.freeze
 export const Reason = Object.freeze({
-    cancle : 'cancle',
-    win : 'win',
-    lose : 'lose',
+   cancle: 'cancle',
+   win: 'win',
+   lose: 'lose',
 });
 
 // Builder Pattern
-export class GameBuilder{
-    setTimerDuration(duration){
-        this.duration = duration;
-        return this;
-    }
+export class GameBuilder {
+   setTimerDuration(duration) {
+      this.duration = duration;
+      return this;
+   }
 
-    setBugCount(num){
-        this.bugCount = num;
-        return this;
-    }
-    
-    setCarrotCount(num){
-        this.carrotCount = num;
-        return this;
-    }
+   setBugCount(num) {
+      this.bugCount = num;
+      return this;
+   }
 
-    build(){
-        console.log(this);
-        return new Game(
-            this.duration,
-            this.bugCount,
-            this.carrotCount,
-        )
-    }
+   setCarrotCount(num) {
+      this.carrotCount = num;
+      return this;
+   }
+
+   build() {
+      return new Game(this.duration, this.bugCount, this.carrotCount);
+   }
 }
 
+class Game {
+   constructor(bugCount, carrotCount, timerSec) {
+      this.bugCount = bugCount;
+      this.carrotCount = carrotCount;
+      this.timerSec = timerSec;
 
-class Game{
-    constructor(bugCount, carrotCount, timerSec) {
-        this.bugCount = bugCount;
-        this.carrotCount = carrotCount;
-        this.timerSec = timerSec;
-        
-        this.gameBtn = document.querySelector('.startBtn');
-        this.gameTimer = document.querySelector('.game__timer');
-        this.gameCounter = document.querySelector('.game__counter');
-        this.gameBtn.addEventListener('click', ()=>{
-            if(this.started){
-                this.stop(Reason.cancle);
-            } else {
-                this.start();
-            }
-        });
+      this.gameBtn = document.querySelector('.startBtn');
+      this.gameTimer = document.querySelector('.game__timer');
+      this.gameCounter = document.querySelector('.game__counter');
+      this.gameBtn.addEventListener('click', () => {
+         if (this.started) {
+            this.stop(Reason.cancle);
+         } else {
+            this.start();
+         }
+      });
 
-        this.gameStaion = new Field(bugCount, carrotCount);
-        this.gameStaion.setClickListener(this.clickField);
+      this.gameStaion = new Field(bugCount, carrotCount);
+      this.gameStaion.setClickListener(this.clickField);
 
-        this.replayBanner = new PopUp();
+      this.replayBanner = new PopUp();
 
+      this.started = false;
+      this.counter = undefined;
+      this.score = 0;
+   }
 
-        this.started = false;
-        this.counter = undefined;
-        this.score = 0;
-    }
+   setClickListener(onclick) {
+      this.onclick = onclick;
+   }
 
-    setClickListener(onclick){
-        this.onclick = onclick;
-    }
+   clickField = target => {
+      if (!this.started) {
+         return;
+      }
+      if (target.matches('.bug__items')) {
+         target.remove();
+         sound.playCarrot();
 
-    clickField = target =>{
-        if(!this.started){
-            return;
-        }
-        if(target.matches('.bug__items')) {
-            target.remove();
-            sound.playCarrot();
+         this.score++;
+         this.updateScoreBoard(this.score);
 
-            this.score++;
-            this.updateScoreBoard(this.score);
+         if (this.score === this.bugCount) {
+            this.stop(Reason.win);
+         }
+      } else if (target.matches('.carrot__items')) {
+         this.stop(Reason.lose);
+      }
+   };
 
-            if(this.score === this.bugCount){
-                this.stop(Reason.win);
-            }
-        } else if(target.matches('.carrot__items')) {
+   start() {
+      this.started = true;
+      this.initGame();
+      this.showStopBtn();
+      this.startTimer();
+      this.showTimerAndScore();
+      sound.playBg();
+   }
+
+   stop(reason) {
+      this.started = false;
+      this.hideStopBtn();
+      this.stopTimer();
+      sound.stopBg();
+      this.gameStaion.moveStop();
+      // console.log(reason);
+      this.onclick && this.onclick(reason);
+   }
+
+   startTimer() {
+      let count = this.timerSec;
+      this.timerUpdate(count);
+      this.counter = setInterval(() => {
+         if (count <= 0) {
             this.stop(Reason.lose);
-        };
-    }
+            return;
+         }
+         this.timerUpdate(--count);
+      }, 1000);
+   }
 
-    start(){
-        this.started = true;
-        this.initGame();
-        this.showStopBtn();
-        this.startTimer();
-        this.showTimerAndScore();
-        sound.playBg();
-    }
+   stopTimer() {
+      this.hideStopBtn();
+      clearInterval(this.counter);
+   }
 
-    stop(reason){
-        this.started = false;
-        this.hideStopBtn();
-        this.stopTimer();
-        sound.stopBg();
-        this.gameStaion.moveStop();
-        // console.log(reason);
-        this.onclick && this.onclick(reason);
-    }
-    
-    startTimer(){
-        let count = this.timerSec;
-        this.timerUpdate(count);
-        this.counter = setInterval(() => {
-            if(count <= 0){
-                this.stop(Reason.lose);
-                return
-            }
-            this.timerUpdate(--count);
-        }, 1000)
-    }
-    
-    stopTimer(){
-        this.hideStopBtn();
-        clearInterval(this.counter);
-    }
-    
-    timerUpdate(time){
-        const seconds = Math.floor(time % 60);
-        this.gameTimer.innerHTML =`00:${String(seconds).padStart(2, 0)}`;
-    }
-    
-    showTimerAndScore(){
-        this.gameTimer.style.visibility = 'visible';
-        this.gameCounter.style.visibility = 'visible';
-    }
-    
-    showStopBtn(){
-        const icon = document.querySelector('.fas');
-        icon.classList.add('fa-stop');
-        icon.classList.remove('fa-play');
-        this.gameBtn.style.visibility = 'visible';
-    }
-    
-    hideStopBtn(){
-        this.gameBtn.style.visibility = 'hidden';
-    }
-    
-    initGame(){
-        this.score = 0;
-        this.gameCounter.innerText = this.bugCount;
-        this.gameStaion.init();
-    }
-    
-    updateScoreBoard(){
-        this.gameCounter.innerText = this.bugCount - this.score;
-    }
+   timerUpdate(time) {
+      const seconds = Math.floor(time % 60);
+      this.gameTimer.innerHTML = `00:${String(seconds).padStart(2, 0)}`;
+   }
 
+   showTimerAndScore() {
+      this.gameTimer.style.visibility = 'visible';
+      this.gameCounter.style.visibility = 'visible';
+   }
+
+   showStopBtn() {
+      const icon = document.querySelector('.fas');
+      icon.classList.add('fa-stop');
+      icon.classList.remove('fa-play');
+      this.gameBtn.style.visibility = 'visible';
+   }
+
+   hideStopBtn() {
+      this.gameBtn.style.visibility = 'hidden';
+   }
+
+   initGame() {
+      this.score = 0;
+      this.gameCounter.innerText = this.bugCount;
+      this.gameStaion.init();
+   }
+
+   updateScoreBoard() {
+      this.gameCounter.innerText = this.bugCount - this.score;
+   }
 }
